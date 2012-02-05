b310f1202f0efdbad877374b2db73375829eed6b
Guillem Mateu <gmateu@esliceu.com>
2012-02-05 23:29:26 +0100

    prova

--- page.php
+++ page.php
@@ -1,5 +1,29 @@
 <?php get_header() ?>
-	
-</body>
-
-</html>
+<div id="nivoslider">
+<img src="http://lorempixum.com/g/920/310" />
+</div><!-- end of nivoslider -->
+<div id="featured">
+    <div class="item">
+        <h3>Quiere saber mas?</h3>
+        <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+    </div><!-- end of item" -->
+        <div class="item">
+        <h3>Quiere saber mas?</h3>
+        <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+    </div><!-- end of item" -->
+        <div class="item">
+        <h3>Quiere saber mas?</h3>
+        <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+    </div><!-- end of item" -->
+</div><!-- end of featured -->
+<div class="news">
+    <img src="http://lorempixum.com/g/290/130" />
+    <h2>Un post que se destaca...</h2>
+    <p>Sed pharetra risus eu nisi molestie aliquet. Donec ipsum eros, sodales ut convallis ac, posuere vitae orci. <a href="#">Leer más</a></p>
+</div>
+<div class="news">
+    <img src="http://lorempixum.com/g/290/130" />
+    <h2>Un post que se destaca...</h2>
+    <p>Sed pharetra risus eu nisi molestie aliquet. Donec ipsum eros, sodales ut convallis ac, posuere vitae orci. <a href="#">Leer más</a></p>
+</div>
+<?php get_sidebar(); get_footer(); ?>
