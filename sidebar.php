e6d9a09a044371cd444b966367f9bf79ec9e6ba9
Guillem Mateu <gmateu@esliceu.com>
2012-02-05 23:21:26 +0100

    afegits sidebar i footer

---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<div id="sidebar">
+<h3>Ultimas noticias</h3>
+<ul>
+    <li>Una noticia en la sidebar<br />Por admin</li>
+    <li>Una noticia en la sidebar<br />Por admin</li>
+    <li>Una noticia en la sidebar<br />Por admin</li>
+    <li>Una noticia en la sidebar<br />Por admin</li>
+</ul>
+</div><!-- end of sidebar -->
