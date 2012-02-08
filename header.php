be3c9bfb6f757e1fcfdbf26bfa7fcf4bfe2b584a
guillem <guillem@llebeig.(none)>
2012-02-08 14:33:18 +0100

    sense banners però a punt

--- header.php
+++ header.php
@@ -13,6 +13,12 @@
     <?php if (function_exists('is_tag')) { if ( is_tag() ) { ?><?php bloginfo('name'); ?> | Archivo por Tag | <?php  single_tag_title("", true); } } ?>
 </title>
 <?php wp_head(); ?>
+<script type="text/javascript">
+jQuery(window).load(function() {
+    jQuery('#slider').nivoSlider();
+});
+</script>
+</head>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
 <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
@@ -24,8 +30,14 @@
 		<div id="logo">
 			<a href="<?php bloginfo('url'); ?>"><img src='<?php bloginfo("template_directory"); ?>/img/logo.png' /></a>
 		</div><!--end of logo-->
-		<div id="nivoslider">
-			<p>nivo</p>
+		<!-- Then somewhere in the <body> section -->
+		<!--<div id="slider" class="nivoSlider">
+		    <img src='<?php bloginfo("template_directory"); ?>/banners/banner1.png' title="Espais per conviure" />
+		    <img src='<?php bloginfo("template_directory"); ?>/banners/banner2.png' alt="" />
+		    <img src='<?php bloginfo("template_directory"); ?>/banners/banner3.png' alt="" />
+		    <!--<a href="http://dev7studios.com"><img src="images/slide2.jpg" alt="" title="#htmlcaption" /></a>
+		    <img src="images/slide3.jpg" alt="" title="This is an example of a caption" />
+		    <img src="images/slide4.jpg" alt="" />-->
 		</div><!-- end of nivoslider -->		
 	</div><!--end of header-->
 	<?php if ( !function_exists('dynamic_sidebar')
