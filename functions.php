abeb658d70dd63e8606523c3a70684d28004b06e
guillem <guillem@llebeig.(none)>
2012-02-08 17:18:19 +0100

    dos menus i logo

--- functions.php
+++ functions.php
@@ -4,18 +4,18 @@ if(function_exists('register_sidebar')){
         'name' => 'Menu Lateral',
         'before_widget' => '<ul id="menu">',
         'after_widget' => '</ul>',
-        'before_title' => '',
-        'after_title' => '',
+        'before_title' => '<div id="titol_dreta">',
+        'after_title' => '</div>',
     ));
 }
 
 if(function_exists('register_sidebar')){
     register_sidebar(array(
         'name' => 'El nostre estil',
-        'before_widget' => '<div id="menuDreta">',
+        'before_widget' => '<div class="menuDreta">',
         'after_widget' => '</div>',
-        'before_title' => '<h1>',
-        'after_title' => '</h1>',
+        'before_title' => '<span id="titol_submenu">',
+        'after_title' => '</span>',
     ));
 }
 
